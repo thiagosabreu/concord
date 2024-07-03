efd5dabf85f9b0d3701de3257a61f03eef0040d9
RichardCordeiro <115822678+RichardCordeiro@users.noreply.github.com>
2024-07-03 11:02:10 -0300

    clerk.ts removed

--- lib/clerk.ts
+++
@@ -1,7 +0,0 @@
-// lib/clerk.ts
-import { createClient } from '@clerk/nextjs';
-
-export const clerk = createClient({
-  apiKey: process.env.NEXT_PUBLIC_CLERK_API_KEY || '',
-  domain: process.env.NEXT_PUBLIC_CLERK_DOMAIN || '',
-});
